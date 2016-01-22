6a3952168481d857d3e0e10c9bd2e8a080d3f0b3
unHealthy <chenzhutian1992@gmail.com>
2016-01-22 21:53:22 +0800

    geoGrap in socialNetwork Tool

--- src/components/ToolListSocialNetwork.vue
+++ src/components/ToolListSocialNetwork.vue
@@ -5,41 +5,40 @@
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="close" class="close" data-dismiss="modal">&times;</button>
+                    <button type="button" class="close" data-dismiss="modal"  aria-label="Close">&times;</button>
                     <h3 id = "social-netwrok-modal-label" class="modal-title">Forum Social Network</h3>
                 </div>
 
                 <!--TODO here directive-->
-                <div ng-show="data" class="modal-body" style="width:1600px">
-                    <div class="col-md-6" style="height:800px">
-                        <div>
-                            <button class="btn btn-default col-md-4" ng-click="clearSelection()">Clear Selection</button>
+                <div class="modal-body" >
+                    <div class="row">
+                        <div class="col-md-6" style="height:800px">
+                            <button class="btn btn-default col-md-4" @click="clearSelection()">Clear Selection</button>
                             <h4 class="col-md-8">{{showInfo}}</h4>
+                            <div id="social-netwrok-geomap" ></div>
+                            <div wordcloud word-cloud-data = "wordCloudData" style="width: 100%; height:48%"></div>
                         </div>
-                        <div graphgeo geodata="geodata" id="graphGeoContainer" selected-country = "selectedCountry" style="width: 100%; height:48%"></div>
-                        <div wordcloud word-cloud-data = "wordCloudData" style="width: 100%; height:48%"></div>
-                    </div>
 
-                    <div class="col-md-6" style="height:800px" >
-                        <div class="container" style="height:30px; width:40%">
-                            <h5 style="text-align:center">Filter users due to activeness</h5>
-                            <div ui-slider min="0" max="4" ng-model="threshold.value" ng-change="changeThreshold()"></div>
-                        </div>
-                        <div graph data = "data" option = "option" countrycode="countrycode" style="width: 100%" call-me="showTooltip"></div>
+                        <div class="col-md-6" style="height:800px" >
+                            <div class="container" style="height:30px; width:40%">
+                                <h5 style="text-align:center">Filter users due to activeness</h5>
+                                <div ui-slider min="0" max="4" ng-model="threshold.value" ng-change="changeThreshold()"></div>
+                            </div>
+                            <div graph data = "data" option = "option" countrycode="countrycode" style="width: 100%"></div>
+                        </div>                        
                     </div>
                 </div>
                 <!--TODO here directive-->
                 
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal" aria-label="Close">Close</button>
                 </div>
             </div>
         </div>
-
     </div>
 
     <!--Button-->
-    <div v-show="data" class="statisitc-icon" data-toggle="modal" data-target="#social-netwrok-modal">
+    <div v-show="data" class="statisitc-icon" @click="createMap()" data-toggle="modal" data-target="#social-netwrok-modal">
         <span class="fa fa-comments"></span>
         <br/>Social Network
     </div>
@@ -48,6 +47,11 @@
 
 
 <script>
+    //thrid party
+    import d3 from 'd3';
+    import topojson from 'topojson';
+    import Datamap from 'datamaps';
+
        //service
     import dataManager from '../service/datamanager.js';
     import communicator from '../service/communicator.js';
@@ -102,13 +106,16 @@
                 countrycode:"",
                 wordCloudData:[],
                 data:null,
+                geodata:null,
                 datacopy:null
             };
 
         },
+        complexObject:{
+            map:null
+        },
         methods:{
             clearSelection(){
-
                 this.wordCloudData=[];
                 this.showInfo="No selection";
                 this.countrycode='-';
@@ -118,6 +125,47 @@
                     this.data = response.data;
                     this.datacopy = response.data;
                 });
+            },
+            selectedCountry(id){
+                communicator(this).emitCode3(id);
+            },
+            createMap(){
+                
+                var self = this;
+                var datas = this.geodata;
+                var color = d3.scale.linear().range(['#edf8b1', '#2c7fb8'])
+                    .domain([0, Math.log(d3.max(datas.map(function (d) {return d.count;})))]);
+
+                var geoData = {};
+                datas.forEach(function(d){
+                    if(d.code3.length != 3) return;
+                    geoData[d.code3] = {id:d.code3,count:d.count,fillColor:color(Math.log(d.count))};
+                });
+
+                if(!this.complexObject.map){
+                    this.complexObject.map = new Datamap({element:document.getElementById('social-netwrok-geomap'),
+                        height: 380, width:400,
+                        fills: {
+                            defaultFill: '#edf8b1' //any hex, color name or rgb/rgba value
+                        },
+                        data:geoData,
+                        geographyConfig:{ 
+                            borderColor: '#dddddd',
+                            popupTemplate: function (geo, data) {
+                                return ['<div class="hoverinfo"><strong>',
+                                    geo.properties.name + ' : ' + data.count,
+                                    '</strong></div>'].join('');
+                            }
+                        },
+                        done: function(datamap) {
+                            datamap.svg.selectAll('.datamaps-subunit').on('click', function(d) {
+                                self.selectedCountry(d.id);
+                            });
+                        }
+                    });
+                } else{
+                    this.complexObject.map.updateChoropleth(geoData);
+                }
             }
         }
     }
