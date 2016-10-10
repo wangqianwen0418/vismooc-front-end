71ceff0c3971f71971dc7fa6bfbd9dd7a639a155
unHealthy <zhutian.chen@outlook.com>
2016-10-10 18:41:15 +0800

    old routes

--- src/service/datamanager.js
+++ src/service/datamanager.js
@@ -1,71 +1,13 @@
-
 import Vue from 'vue';
 import VueResource from 'vue-resource';
 Vue.use(VueResource);
 
-var mainPath = 'http://localhost:3004/';
-var $http = Vue.http;
+let mainPath = 'http://localhost:3004/';
+let $http = Vue.http;
 
-var getSentiment = function (courseId, callback) {
-    //var tmpURL = mainPath + 'getSentiment?courseId=' + courseId;
-    var tmpURL = mainPath + 'sentiment?courseId=' + courseId;
-    $http.get(tmpURL).then(function (response) {
-        callback(response);
-    });
-};
-
-var getSentimentDetails = function (courseId, days, callback) {
-    var tmpURL = mainPath + 'sentiment?courseId=' + courseId + '&days=' + days;
-    $http.get(tmpURL).then(function (response) {
-        callback(response);
-    });
-};
-
-var getWordCloudDataByUser = function (courseId, userId, callback) {
-    //var tmpURL = mainPath + 'getWordCloudData?courseId=' + courseId + '&userId=' + userId;
-    var tmpURL = mainPath + 'wordCloudData?type=user&courseId=' + courseId + '&userId=' + userId;
-    $http.get(tmpURL).then(function (response) {
-        callback(response);
-    });
-};
-
-var getWordCloudDataByGeo = function (courseId, countryCode, callback) {
-    var tmpURL = mainPath + 'getWordCloudData?type=geo&courseId=' + courseId + '&countryCode=' + countryCode;
-    $http.get(tmpURL).then(function (response) {
-        callback(response);
-    });
-};
-
-var getForumSocialNetwork = function (courseId, filterLevel, callback) {
-    if (filterLevel === 0 || filterLevel) {
-    } else {
-        filterLevel = 0;
-    }
-    var tmpURL = mainPath + 'modules/vistoolkit/data/graph/' + courseId + '-' + filterLevel + ".json";
-    $http.get(tmpURL)
-        .then(function (response) {
-            if (response.ok)
-                callback(response);
-        });
-};
-
-var getSeekInfo = function (courseId, videoId, paramters, callback) {
-    var tmpURL = mainPath + 'contentBasedData?type=seek&courseId=' + courseId + '&videoId=' + videoId;
-    if (typeof paramters === 'function') {
-        callback = paramters;
-    }else if (typeof paramters === 'object') {
-        for (let p in paramters) {
-            tmpURL += '&' + p + '=' + paramters[p];
-        }
-    }
-
-    $http.get(tmpURL).then(function (response) {
-        callback(response);
-    });
-};
-
-var getActionCountInfo = function (courseId, videoId, paramters, callback) {
-    var tmpURL = mainPath + 'contentBasedData?type=action&courseId=' + courseId + '&videoId=' + videoId;
+// We need
+let getActionCountInfo = function (courseId, videoId, paramters, callback) {
+    let tmpURL = mainPath + 'getContentBasedData?type=action&courseId=' + courseId + '&videoId=' + videoId;
     if (typeof paramters === 'function') {
         callback = paramters;
     }else if (typeof paramters === 'object') {
@@ -80,43 +22,30 @@ var getActionCountInfo = function (courseId, videoId, paramters, callback) {
 
 };
 
-var getDailyHotnessByVideo = function (courseId, videoId, callback) {
-    var tmpURL = mainPath + 'videoPop?courseId=' + courseId + '&videoId=' + videoId;
+let getSeekInfo = function (courseId, videoId, paramters, callback) {
+    let tmpURL = mainPath + 'getContentBasedData?type=seek&courseId=' + courseId + '&videoId=' + videoId;
+    if (typeof paramters === 'function') {
+        callback = paramters;
+    }else if (typeof paramters === 'object') {
+        for (let p in paramters) {
+            tmpURL += '&' + p + '=' + paramters[p];
+        }
+    }
+
     $http.get(tmpURL).then(function (response) {
         callback(response);
     });
 };
 
-var getDemographicData = function (courseId, callback) {
-    var tmpURL = mainPath + 'demographicData?courseId=' + courseId;
+let getCourseList = function (callback) {
+    let tmpURL = mainPath + 'getCourseList';
     $http.get(tmpURL).then(function (response) {
         callback(response);
     });
 };
 
-var getCourseInfo = function (courseId, callback) {
-    var tmpURL = mainPath + 'courseInfo?courseId=' + courseId;
-    $http.get(tmpURL).then(function (response) {
-        callback(response);
-    });
-};
-
-var getCourseList = function (callback) {
-    var tmpURL = mainPath + 'courseList';
-    $http.get(tmpURL).then(function (response) {
-        callback(response);
-    });
-};
-
-var getHotness = function (courseId, callback) {
-    var tmpURL = mainPath + 'hotness?courseId=' + courseId;
-    $http.get(tmpURL).then(function (response) {
-        callback(response);
-    });
-};
-
-var getVideoList = function (courseId, callback) {
-    var tmpURL = mainPath + 'videoList?courseId=' + courseId;
+let getVideoList = function (courseId, callback) {
+    let tmpURL = mainPath + 'getVideoList?courseId=' + courseId;
     $http.get(tmpURL).then(function (response) {
         response.data.forEach(function (week) {
             week.videos.forEach(function (video) {
@@ -129,8 +58,78 @@ var getVideoList = function (courseId, callback) {
     });
 };
 
-var getAnimationTest = function (courseId, videoId, startTime, endTime, callback) {
-    var tmpURL = mainPath + 'animationtest?videoId=' + videoId + '&courseId=' + courseId + '&startTime=' + startTime + '&endTime=' + endTime;
+let getCourseInfo = function (courseId, callback) {
+    let tmpURL = mainPath + 'getCourseInfo?courseId=' + courseId;
+    $http.get(tmpURL).then(function (response) {
+        callback(response);
+    });
+};
+
+let getHotness = function (courseId, callback) {
+    let tmpURL = mainPath + 'getHotness?courseId=' + courseId;
+    $http.get(tmpURL).then(function (response) {
+        callback(response);
+    });
+};
+
+let getDemographicData = function (courseId, callback) {
+    let tmpURL = mainPath + 'getDemographicData?courseId=' + courseId;
+    $http.get(tmpURL).then(function (response) {
+        callback(response);
+    });
+};
+let getDailyHotnessByVideo = function (courseId, videoId, callback) {
+    let tmpURL = mainPath + 'getVideoPop?courseId=' + courseId + '&videoId=' + videoId;
+    $http.get(tmpURL).then(function (response) {
+        callback(response);
+    });
+};
+
+// no need currently
+let getSentiment = function (courseId, callback) {
+    let tmpURL = mainPath + 'getSentiment?courseId=' + courseId;
+    $http.get(tmpURL).then(function (response) {
+        callback(response);
+    });
+};
+
+let getWordCloudData = function (courseId, userId, callback) {
+    let tmpURL = mainPath + 'getWordCloudData?courseId=' + courseId + '&userId=' + userId;
+
+    $http.get(tmpURL).then(function (response) {
+        callback(response);
+    });
+};
+
+let getWordCloudDataByGeo = function (courseId, code3, callback) {
+    let tmpURL = mainPath + 'getWordCloudDataByGeo?courseId=' + courseId + '&countryCode=' + code3;
+    $http.get(tmpURL).then(function (response) {
+        callback(response);
+    });
+};
+
+let getSentimentDetails = function (courseId, days, callback) {
+    let tmpURL = mainPath + 'getSentimentDetails?courseId=' + courseId + '&days=' + days;
+    $http.get(tmpURL).then(function (response) {
+        callback(response);
+    });
+};
+
+let getForumSocialNetwork = function (courseId, filterLevel, callback) {
+    if (filterLevel === 0 || filterLevel) {
+    } else {
+        filterLevel = 0;
+    }
+    let tmpURL = mainPath + 'data/forumSocialNetwork/' + courseId + '-' + filterLevel + ".json";
+    $http.get(tmpURL)
+        .then(function (response) {
+            if (response.ok)
+                callback(response);
+        });
+};
+
+let getAnimationTest = function (courseId, videoId, startTime, endTime, callback) {
+    let tmpURL = mainPath + 'animationtest?videoId=' + videoId + '&courseId=' + courseId + '&startTime=' + startTime + '&endTime=' + endTime;
     $http.get(tmpURL)
         .then(function (response) {
             callback(response);
@@ -138,16 +137,16 @@ var getAnimationTest = function (courseId, videoId, startTime, endTime, callback
     ;
 };
 
-var getGlyphInfo = function (courseId, callback) {
-    var tmpURL = mainPath + 'glyphInfo?courseId=' + courseId;
+let getGlyphInfo = function (courseId, callback) {
+    let tmpURL = mainPath + 'getGlyphInfo?courseId=' + courseId;
     $http.get(tmpURL)
         .then(function (response) {
             callback(response);
         });
 };
 
-var getParallelCoor = function (courseId, callback) {
-    var tempURL = mainPath + 'parallelCoor?courseId=' + courseId;
+let getParallelCoor = function (courseId, callback) {
+    let tempURL = mainPath + 'ParallelCoor?courseId=' + courseId;
     $http.get(tempURL)
         .then(function (response) {
             callback(response)
@@ -155,15 +154,13 @@ var getParallelCoor = function (courseId, callback) {
     ;
 };
 
-
-
 // Public API
 export default {
     'getParallelCoor': getParallelCoor,
     'getGlyphInfo': getGlyphInfo,
     'getAnimationTest': getAnimationTest,
     'getSentiment': getSentiment,
-    'getWordCloudDataByUser': getWordCloudDataByUser,
+    'getWordCloudData': getWordCloudData,
     'getWordCloudDataByGeo': getWordCloudDataByGeo,
     'getSentimentDetails': getSentimentDetails,
     'getForumSocialNetwork': getForumSocialNetwork,
